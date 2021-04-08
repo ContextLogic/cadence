4d4ba1e7f71ac01f45d1d825d206cb644bba8825
bgao-wish <bgao@wish.com>
2021-04-07 21:01:23 -0700

    resolve conflict

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/ContextLogic/cadence/pkg/fsm/workflow"
 	"github.com/ContextLogic/cadence/pkg/temporal"
 	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/client"
@@ -55,41 +56,43 @@ func getWorkflows(path string) []*Workflow {
 }
 
 func main() {
-	e := getWorkflow("./cmd/workflow.json")
-	w, err := wf.New(e.json)
-	if err != nil {
-		panic(fmt.Errorf("error loading workflow %w", err))
-	}
-	logger.WithFields(logrus.Fields{"workflow": w}).Info("workflows")
+	wfs := getWorkflows("./cmd/workflows.json")
+	for _, wf := range wfs {
+		w, err := workflow.New(wf.json)
+		if err != nil {
+			panic(fmt.Errorf("error loading workflow %w", err))
+		}
+		logger.WithFields(logrus.Fields{"workflow": w}).Info("workflows")
 
-	activityMap := map[string]func(context.Context, interface{}) (interface{}, error){
-		"example:activity:Activity1": Activity1,
-		"example:activity:Activity2": Activity2,
-	}
+		activityMap := map[string]func(context.Context, interface{}) (interface{}, error){
+			"example:activity:Activity1": Activity1,
+			"example:activity:Activity2": Activity2,
+		}
 
-	w.RegisterWorkflow(e.name)
-	w.RegisterActivities(activityMap)
-	w.RegisterWorker()
-	w.RegisterTaskHandlers(activityMap)
+		w.RegisterWorkflow(wf.name)
+		w.RegisterActivities(activityMap)
+		w.RegisterWorker()
+		w.RegisterTaskHandlers(activityMap)
 
-	instance, err := temporal.BaseClient.ExecuteWorkflow(
-		context.Background(),
-		client.StartWorkflowOptions{
-			ID:        strings.Join([]string{"cadence_lib", strconv.Itoa(int(time.Now().Unix()))}, "_"),
-			TaskQueue: "TASK_QUEUE_cadence_lib",
-		},
-		e.name,
-		map[string]interface{}{
-			"foo": 1,
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-	response := map[string]interface{}{}
-	err = instance.Get(context.Background(), &response)
-	if err != nil {
-		panic(err)
+		instance, err := temporal.BaseClient.ExecuteWorkflow(
+			context.Background(),
+			client.StartWorkflowOptions{
+				ID:        strings.Join([]string{"cadence_lib", strconv.Itoa(int(time.Now().Unix()))}, "_"),
+				TaskQueue: "TASK_QUEUE_cadence_lib",
+			},
+			wf.name,
+			map[string]interface{}{
+				"foo": 1,
+			},
+		)
+		if err != nil {
+			panic(err)
+		}
+		response := map[string]interface{}{}
+		err = instance.Get(context.Background(), &response)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
@@ -98,7 +101,7 @@ func Activity1(ctx context.Context, input interface{}) (interface{}, error) {
 	taskToken := string(activityInfo.TaskToken)
 	activityName := activityInfo.ActivityType.Name
 
-	logger.WithFields(logrus.Fields{"input": input, "taskToken": taskToken, "activityName": activityName}).Info("activity1 executed")
+	logger.WithFields(logrus.Fields{"input": input, "taskToken": taskToken, "activityName": activityName}).Info("activity executed")
 
 	return input, nil
 }
@@ -108,7 +111,7 @@ func Activity2(ctx context.Context, input interface{}) (interface{}, error) {
 	taskToken := string(activityInfo.TaskToken)
 	activityName := activityInfo.ActivityType.Name
 
-	logger.WithFields(logrus.Fields{"input": input, "taskToken": taskToken, "activityName": activityName}).Info("activity2 executed")
+	logger.WithFields(logrus.Fields{"input": input, "taskToken": taskToken, "activityName": activityName}).Info("activity executed")
 
 	return input, nil
 }
